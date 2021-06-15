dc17f987e4ccaa236a45c5201e424cb2443963ad
dojo24 <melissalongenberger24@gmail.com>
2021-06-15 19:15:42 -0400

    adding js file for office hours if needed

---
+++ OfficeHours/scripts.js
@@ -0,0 +1,7 @@
+// Pseudo code is the practice of understanding what you are going to code before you actually code by using everyday words to write the problem.  For example:
+
+// Find the length of the following array: 2,4,5,8,10
+// My pseudo code would look like this:  
+// I am given an array or list of something and need to find how many entries are in it
+
+// Then I would code it
